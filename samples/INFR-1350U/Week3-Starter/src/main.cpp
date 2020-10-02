99b15518a3147435db822e08af4d96e2a3ce45b5
Maddin Thom <maddin.thom@ontariotechu.net>
2020-10-02 10:06:56 -0400

    final adjustments

--- samples/INFR-1350U/Week3-Starter/src/main.cpp
+++ samples/INFR-1350U/Week3-Starter/src/main.cpp
@@ -64,7 +64,7 @@ bool initGLFW() {
 	}
 
 	//Create a new GLFW window
-	window = glfwCreateWindow(800, 800, "INFR1350U", nullptr, nullptr);
+	window = glfwCreateWindow(800, 800, "Thom_Maddin_100751351", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	// Set our window resized callback
